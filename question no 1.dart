0ff4364b2f5c8b43410f44e747add444b0020e98
Abdullah-khan07 <abdullahahmedkhan072@gmail.com>
2024-12-08 01:29:52 -0800

    Answer is qustion 1

--- question no 1.dart
+++ question no 1.dart
@@ -1,3 +1,14 @@
 void main (){
-  
+  void main() {
+  int length = 55;
+  int breadth = 55;
+  bool Answer = (length == breadth);
+  if (length == breadth) {
+    print(" Square");
+  } else {
+    ("Rectangle");
+  }
+}
+
+
 }
