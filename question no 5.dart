2b8536370dca95f10488001de6feca0759c2d487
Abdullah-khan07 <abdullahahmedkhan072@gmail.com>
2024-12-08 01:43:59 -0800

    question no 5

---
+++ question no 5.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+void main() {
+  
+  print('Enter temperature in Celsius:');
+  num temperature = num.parse(stdin.readLineSync()!);
+  if (temperature < 0) {
+    print('Freezing cold!');
+  } else if (temperature >= 0 && temperature < 10) {
+    print('Very cold!');
+  } else if (temperature >= 10 && temperature < 20) {
+    print('Cold');
+  } else if (temperature >= 20 && temperature < 30) {
+    print('Warm');
+  } else if (temperature >= 30 && temperature < 40) {
+    print('Hot');
+  } else {
+    print('Very hot');
+  }
+}
