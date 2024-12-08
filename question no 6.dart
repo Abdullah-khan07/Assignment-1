c261b12b3730b4e8d130c5c2c8f5f85716571657
Abdullah-khan07 <abdullahahmedkhan072@gmail.com>
2024-12-08 01:45:19 -0800

    question no 6

---
+++ question no 6.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void main() {
+  String checkvovel = ("e");
+  if (checkvovel == "a" ||
+  checkvovel == "e" || 
+  checkvovel == "i" ||
+  checkvovel == "o" ||) {
+  
+    print("It is a vovel");
+  }
+   else {
+    ("It is a constant");
+  }
+  
+}
