dc21d16cb1dd237d28f37119991e431b9f1a6d90
Abdullah-khan07 <abdullahahmedkhan072@gmail.com>
2024-12-08 01:37:36 -0800

    question no 2

--- paractice.dart
+++
@@ -1,6 +0,0 @@
-void main() {
-  int name = 56;
-  int kasmir = 10;
-  int sum = name * kasmir;
-  print(sum);
-}
---
+++ qustion no 2.dart
@@ -0,0 +1,10 @@
+void main() {
+  int Abdullah = 18;
+  int Huzaifa = 13;
+  bool age = (Abdullah > Huzaifa);
+  if (Abdullah > Huzaifa) {
+    print("Abdullah is youngest");
+  } else {
+    print("Huzifa is youngest");
+  }
+}
